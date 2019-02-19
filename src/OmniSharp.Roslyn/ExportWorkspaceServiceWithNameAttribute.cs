f5b04dd7a7d6718cddda46905db7feb1d986ebf2
Savpek <savolainen.pekka@gmail.com>
2019-02-19 21:25:57 +0200

    It actually works O_o

--- src/OmniSharp.Roslyn/ExportWorkspaceServiceWithNameAttribute.cs
+++ src/OmniSharp.Roslyn/ExportWorkspaceServiceWithNameAttribute.cs
@@ -2,6 +2,7 @@ using System;
 using System.Composition;
 using System.Dynamic;
 using System.Reflection;
+using Castle.DynamicProxy;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Host.Mef;
 
@@ -28,13 +29,13 @@ namespace OmniSharp
 
     [MetadataAttribute]
     [AttributeUsage(AttributeTargets.Class)]
-    public class ExportWorkspaceServiceWithAssemblyQualifiedName2Attribute : ExportWorkspaceServiceAttribute
+    public class ExportWorkspaceServiceFactoryWithAssemblyQualifiedNameAttribute : ExportAttribute
     {
-        public new string ServiceType { get; }
-        public new string Layer { get; }
+        public string ServiceType { get; }
+        public string Layer { get; }
 
-        public ExportWorkspaceServiceWithAssemblyQualifiedName2Attribute(string typeAssembly, string typeName, string layer = ServiceLayer.Host)
-            : base(typeof(IWorkspaceService))
+        public ExportWorkspaceServiceFactoryWithAssemblyQualifiedNameAttribute(string typeAssembly, string typeName, string layer = ServiceLayer.Host)
+            : base(typeof(IWorkspaceServiceFactory))
         {
             var type = Assembly.Load(typeAssembly).GetType(typeName)
                 ?? throw new InvalidOperationException($"Could not resolve '{typeName} from '{typeAssembly}'");
@@ -54,25 +55,34 @@ namespace OmniSharp
     {
     }
 
-    [Shared]
-    [ExportWorkspaceServiceWithAssemblyQualifiedName("Microsoft.CodeAnalysis.Features", "Microsoft.CodeAnalysis.PickMembers.IPickMembersService")]
-    public class FooBarJeeJee : DynamicObject, IWorkspaceService
+    public class SomeInterceptor : IInterceptor
     {
-        public FooBarJeeJee()
+        public void Intercept(IInvocation invocation)
         {
-            Console.WriteLine("MITÄSVITTUATAAS");
-        }
-
-        public override bool TryConvert(ConvertBinder binder, out object result)
-        {
-            result = null;
-            return !binder.Type.IsValueType;
-        }
-
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-        {
-            Console.WriteLine("ASDFASDFASDF");
-            throw new InvalidOperationException("VOESAATANA");
+            var resultTypeInternal = Assembly.Load("Microsoft.CodeAnalysis.Features").GetType("Microsoft.CodeAnalysis.PickMembers.PickMembersResult");
+            var resultInstance = Activator.CreateInstance(resultTypeInternal, new object[] { invocation.Arguments[1], invocation.Arguments[2] });
+            invocation.ReturnValue = resultInstance;
         }
     }
+
+    [Shared]
+    [ExportWorkspaceServiceFactoryWithAssemblyQualifiedName("Microsoft.CodeAnalysis.Features", "Microsoft.CodeAnalysis.PickMembers.IPickMembersService")]
+    public class FooBarJeeJee : IWorkspaceServiceFactory
+    {
+        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
+        {
+            ProxyGenerator generator = new ProxyGenerator();
+            var internalType = Assembly.Load("Microsoft.CodeAnalysis.Features").GetType("Microsoft.CodeAnalysis.PickMembers.IPickMembersService");
+            // var vsDummyType = Assembly.Load("Microsoft.VisualStudio.LanguageServices").GetType("Microsoft.VisualStudio.LanguageServices.Implementation.PickMembers.VisualStudioPickMembersService");
+            // var dummyService = Activator.CreateInstance(vsDummyType, new object[] { null });
+            return (IWorkspaceService)generator.CreateInterfaceProxyWithoutTarget(internalType, new[] { typeof(IWorkspaceService)}, new SomeInterceptor());
+        }
+    }
+
+    // [Shared]
+    // [ExportWorkspaceServiceWithAssemblyQualifiedName("Microsoft.CodeAnalysis.Features", "Microsoft.CodeAnalysis.PickMembers.IPickMembersService")]
+    // public class FooBarJeeJee : IWorkspaceService
+    // {
+
+    // }
 }
